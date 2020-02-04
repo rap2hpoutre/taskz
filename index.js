9129d91246ee275ccc1f714112d86f3c951c268c
Shreyansh K Mehta <shreyansh.kumar.mehta@sap.com>
2020-02-04 23:04:25 +0530

    dep(chalk-kleur): migrated to kleur from chalk

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const chalk = require("chalk");
+const {grey, reset} = require("kleur");
 const Spinnies = require("spinnies");
 
 function taskz(tasks, options = { parallel: false }) {
@@ -48,7 +48,7 @@ async function runSequence(tasks, level, ctx, spinnies) {
 
 async function runTask({ t, i, level, ctx, spinnies, tasks }) {
   const id = String(i + Math.random());
-  const counter = chalk.dim.grey(` [${i + 1}/${tasks.length}]`);
+  const counter = grey().dim(` [${i + 1}/${tasks.length}]`);
   spinnies.add(id, { text: `${t.text}${counter}`, indent: level });
   ctx.text = val => {
     spinnies.update(id, { text: `${val}${counter}` });
@@ -64,9 +64,9 @@ async function runTask({ t, i, level, ctx, spinnies, tasks }) {
     // Run one task
     try {
       await t.task(ctx);
-      spinnies.succeed(id, { text: chalk.reset(spinnies.pick(id).text) });
+      spinnies.succeed(id, { text: reset(spinnies.pick(id).text) });
     } catch (e) {
-      spinnies.fail(id, { text: chalk.reset(`${e.message}${counter}`) });
+      spinnies.fail(id, { text: reset(`${e.message}${counter}`) });
       if (t.stopOnError) {
         throw new Error("Stopped on error.");
       }
